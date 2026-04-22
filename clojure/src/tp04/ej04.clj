c1ae769a12fc4a914c66c794e2d2bfeb96fd8716
matisar05 <87400624+NachSarasola@users.noreply.github.com>
2026-04-22 19:53:24 -0300

    CLJ: Hasta el 7

--- clojure/src/tp04/ej04.clj
+++ clojure/src/tp04/ej04.clj
@@ -2,16 +2,22 @@
   "Ejercicio 4 — Pipeline con ->> (5 pts). Trazabilidad: F-08")
 
 ;; Filtra activas, extrae :total, suma.
-(defn total-activas [ordenes]
-  ;; TODO: implementar con ->>
-  )
+((defn total-activas [ordenes]
+  (->> ordenes 
+        (filter:activa?)
+        (map:total))
+        (reduce + 0)))
 
 ;; Filtra activas, devuelve vector de :cliente.
 (defn nombres-activas [ordenes]
-  ;; TODO: implementar con ->>
-  )
+  (->> ordenes
+        (filter:activa?)
+        (map:cliente)
+        (vec)))
 
 ;; Filtra pares, eleva al cuadrado, suma.
 (defn cuadrados-pares [nums]
-  ;; TODO: implementar con ->>
-  )
+  (->> nums
+      (filter even?)
+      (map #(* % %))
+      (reduce + 0)))
--- clojure/src/tp04/ej07.clj
+++ clojure/src/tp04/ej07.clj
@@ -4,21 +4,23 @@
 
 ;; Retorna {:status :ok :value value} si no vacío, {:status :error :error "FIELD es obligatorio"}.
 (defn required-field [field-name value]
-  ;; TODO: implementar
+  (if (and (not (nil? value))  (not (str/blank? (str value))))
+    {:status :ok :value value}
+    {:status :error :error (str field-name " es obligatorio")})
   )
 
 (def doble
-  ;; TODO: (partial * 2)
+  (partial * 2)
   )
 
 (def triple
-  ;; TODO: (partial * 3)
+  (partial * 3)
   )
 
 (def validate-name
-  ;; TODO: (partial required-field "nombre")
+  (partial required-field "nombre")
   )
 
 (def validate-email
-  ;; TODO: (partial required-field "email")
+  (partial required-field "email")
   )
